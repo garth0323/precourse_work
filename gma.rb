a56ce258a7a8e282e740ee4759c960f9098aba76
garth0323 <garth0323@gmail.com>
2013-12-30 06:41:14 +0000

    more exercises

--- gma.rb
+++ gma.rb
@@ -1,13 +1,13 @@
 puts 'Ask Deaf Grandma Anything'
- 
-while true	   
-      question = gets.chomp
+question = gets.chomp
+while question != 'bye'	         
       if question != question.upcase
         puts 'HUH?! SPEAK UP, SONNY!'
+        question = gets.chomp
        else question == question.upcase
 	      puts 'NO, NOT SINCE ' + Random.new.rand(1900..1950).to_s
+        question = gets.chomp
        end
-break if question == 'bye'
 end
 
 
