8acd9f324734025dc955c7270cc521df2ccbdcc6
garth0323 <garth0323@gmail.com>
2014-01-07 04:35:14 +0000

    logger

---
+++ learn_to_program/logger.rb
@@ -0,0 +1,15 @@
+def log description, &block
+  puts "Starting #{description}..."
+  block.call
+  puts "#{description} is done."
+end
+
+log 'outside' do
+  puts "Starting inside block"
+  puts '5'
+  
+  log 'inside' do
+    puts "I like Thai Food!"
+  end
+  
+end
